f9ad89ba6a9902c2406a81d44c49f685b69f546e
krausvo1 <vojta.krauz@seznam.cz>
2017-06-05 00:14:35 +0100

    Add files via upload

---
+++ border.cpp
@@ -0,0 +1,8 @@
+#include "border.h"
+
+TBorder::TBorder(const int & ypos, 
+				 const int & xpos) : 
+		
+				 t_ypos(ypos), t_xpos(xpos) 
+{
+}
---
+++ log.cpp
@@ -0,0 +1,8 @@
+#include "log.h"
+
+TLog::TLog(const int & ID, 
+	       const int & health) : 
+	
+	       t_ID(ID), t_health(health) 
+{
+}
